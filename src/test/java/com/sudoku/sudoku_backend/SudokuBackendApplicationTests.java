70bafbec9ee1b64403db62fb1695784ce6b70a7a
jonnyell89 <jonnyell89@gmail.com>
2026-06-15 14:25:21 +0100

    Initial commit - Spring Boot setup

---
+++ src/test/java/com/sudoku/sudoku_backend/SudokuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudoku.sudoku_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

93444c10c9de09bec55eb97a1bd0d14244e7e5d0
jonnyell89 <jonnyell89@gmail.com>
2026-06-15 15:13:48 +0100

    SudokuConstants class added.

---
+++ src/main/java/com/sudoku/sudoku_backend/SudokuConstants.java
@@ -0,0 +1,14 @@
+package com.sudoku.sudoku_backend;
+
+public final class SudokuConstants {
+
+    private SudokuConstants() {}
+
+    public static final int GRID_SIZE = 9;
+    public static final int BOX_SIZE = 3;
+    public static final int EMPTY_CELL = 0;
+    public static final int MIN_INDEX = 0;
+    public static final int MAX_INDEX = GRID_SIZE - 1;
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = GRID_SIZE;
+}

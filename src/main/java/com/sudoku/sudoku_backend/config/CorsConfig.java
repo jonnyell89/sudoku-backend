f9467886307cc0a32ba5410b44bb32d933421211
jonnyell89 <jonnyell89@gmail.com>
2026-06-15 15:11:47 +0100

    config package added, CorsConfig class added.

---
+++ src/main/java/com/sudoku/sudoku_backend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.sudoku.sudoku_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Cross-Origin Resource Sharing is a browser security mechanism that blocks your frontend port 5173 from talking to your backend port 8080 by default.
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // Allow endpoints that start with /api/.
+                .allowedOrigins("http:localhost:5173") // Allow requests from the frontend server.
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow all CRUD operations.
+    }
+}

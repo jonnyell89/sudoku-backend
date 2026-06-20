3d1e1fb020db13b6fb1c4b928ae243a6e0b42471
jonnyell89 <jonnyell89@gmail.com>
2026-06-20 14:43:53 +0100

    validateGrid method added to Solver class.

--- src/main/java/com/sudoku/sudoku_backend/core/Solver.java
+++ src/main/java/com/sudoku/sudoku_backend/core/Solver.java
@@ -10,10 +10,13 @@ public class Solver {
 //
 //    private int[] findEmptyCell(Grid grid) {}
 
-    private void gridValidation(Grid grid) {
+    private void validateGrid(Grid grid) {
 
         if (grid == null) {
             throw new IllegalArgumentException("grid should not be null.");
+        }
+        if (!grid.isValid()) {
+            throw new IllegalArgumentException("grid must not contain duplicate values in any row, col, or box.");
         }
     }
 }

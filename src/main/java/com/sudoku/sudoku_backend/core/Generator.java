96be220b9ffe60e2a97a1be8527a3d0d445c0e57
jonnyell89 <jonnyell89@gmail.com>
2026-07-05 16:06:42 +0100

    Generator class added.

---
+++ src/main/java/com/sudoku/sudoku_backend/core/Generator.java
@@ -0,0 +1,12 @@
+package com.sudoku.sudoku_backend.core;
+
+import java.util.Random;
+
+public class Generator {
+
+    private final Random random;
+
+    public Generator(Random random) {
+        this.random = random;
+    }
+}
